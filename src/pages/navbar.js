31839e02a316504bf4822010eb0743f700faf2d1
Spencer Stice <smstice17@gmail.com>
2022-12-22 13:52:59 -0800

    final commit

--- src/pages/navbar.js
+++ src/pages/navbar.js
@@ -7,7 +7,7 @@ function Navbar() {
         <a href="/">
             <img src={require("../static/images/Senior Photo_Square.jpg")} alt="Spencer" class="spencer-image"/>
         </a>
-        <a className="navbar-left" href="/">Spencer Stice</a>
+        <a className="navbar-left" href="/Personal-Portfolio-Website/#/">Spencer Stice</a>
       <ul className="navbar-nav">
         <li className="nav-item">
           <a className="nav-link" href="/Personal-Portfolio-Website/#/projects">Projects</a>
