c88b8dcd5ee9aea28d88db520804258f2b2bb133
Spencer Stice <smstice17@gmail.com>
2023-06-23 12:54:31 -0700

    Updating classes and changing to columns from rows

--- src/pages/classes.js
+++ src/pages/classes.js
@@ -7,7 +7,7 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import {Helmet} from 'react-helmet';
+import { Helmet } from 'react-helmet';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import '../styles/classes.css';
 import { DonutLarge } from '@mui/icons-material';
@@ -24,76 +24,83 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
 }));
 
 const theme = createTheme({
-    palette: {
-      primary: {
-        main: "#f56702",
-      },
+  palette: {
+    primary: {
+      main: "#f56702",
     },
-  });
-
-const StyledTableRow = styled(TableRow)(({ theme }) => ({
-  '&:nth-of-type(odd)': {
-    backgroundColor: theme.palette.action.hover,
   },
-  // hide last border
-  '&:last-child td, &:last-child th': {
-    border: 0,
-  },
-}));
+});
 
-function createData(
-  cs: string,
-  math: string,
-  ee: string,
-  physics: string,
-  misc: string,
-) {
-  return { cs, math, ee, physics, misc };
-}
+const csClasses = [
+  'Artificial Intelligence',
+  'Machine Learning',
+  'Algorithms and Complexity',
+  'Operating Systems',
+  'Computer Networking',
+  'Software Construction (github, linux, etc)',
+];
 
-const rows = [
-  createData('Machine Learning', 'Linear Algebra', 'Transistor Circuits', 'Circuits and Electromagnetism Lab', 'Astrobiology'),
-  createData('Algorithms', 'Differential Equations', 'Systems and Signals', 'Electromagnetism and Relativity', 'History of Rock and Roll'),
-  createData('Operating Systems', 'Multivariable Calculus', 'Logic Design of Digital Systems', 'Fluids and EM', 'History of Africa'),
-  createData('Software Construction Lab', '', 'Intro to Electrical Engineering', 'Mechanics', ''),
-  createData('Computer Organization', '', 'Engineering Design: Internet of Things', '', ''),
-  createData('Intro to Computer Science', '', '', '', ''),
+const mathClasses = [
+  'Upper Division Linear Algebra',
+  'Multivariable Calculus',
+  'Lower Division Linear Algebra',
+  'Differential Equations',
+  'Theory of Interest (finance)',
+];
+
+const eeClasses = [
+  'Transistor Circuits',
+  'Computer Organization',
+  'Systems and Signals (fourier)',
+  'Logic Design',
+  'Verilog FPGA Lab',
+  'Engineering Design: Internet of Things',
+];
+
+const physicsClasses = [
+  'Circuits and Electromagnetism Lab',
+  'Electromagnetism and Relativity',
+  'Fluids and EM',
+  'Mechanics',
+];
+
+const miscClasses = [
+  'Astrobiology',
+  'History of Rock and Roll',
 ];
 
 export default function Classes() {
   return (
-    <div class="classes-div">
-        <ThemeProvider theme={theme}>
+    <div className="classes-div">
+      <ThemeProvider theme={theme}>
         <Helmet>
-            <style>{'body { background-color: black; }'}</style>
+          <style>{'body { background-color: black; }'}</style>
         </Helmet>
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 700 }} aria-label="customized table">
-        <TableHead>
-          <TableRow>
-            <StyledTableCell>Computer Science</StyledTableCell>
-            <StyledTableCell >Math</StyledTableCell>
-            <StyledTableCell >Electrical Engineering</StyledTableCell>
-            <StyledTableCell >Physics</StyledTableCell>
-            <StyledTableCell >Misc</StyledTableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <StyledTableRow key={row.cs}>
-              <StyledTableCell component="th" scope="row">
-                {row.cs}
-              </StyledTableCell>
-              <StyledTableCell >{row.math}</StyledTableCell>
-              <StyledTableCell >{row.ee}</StyledTableCell>
-              <StyledTableCell >{row.physics}</StyledTableCell>
-              <StyledTableCell >{row.misc}</StyledTableCell>
-            </StyledTableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-    </ThemeProvider>
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 700 }} aria-label="customized table">
+            <TableHead>
+              <TableRow>
+                <StyledTableCell>Computer Science</StyledTableCell>
+                <StyledTableCell>Math</StyledTableCell>
+                <StyledTableCell>Electrical Engineering</StyledTableCell>
+                <StyledTableCell>Physics</StyledTableCell>
+                <StyledTableCell>Misc</StyledTableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {Array.from({ length: Math.max(csClasses.length, mathClasses.length, eeClasses.length, physicsClasses.length, miscClasses.length) }).map((_, index) => (
+                <TableRow key={index}>
+                  <StyledTableCell>{csClasses[index]}</StyledTableCell>
+                  <StyledTableCell>{mathClasses[index]}</StyledTableCell>
+                  <StyledTableCell>{eeClasses[index]}</StyledTableCell>
+                  <StyledTableCell>{physicsClasses[index]}</StyledTableCell>
+                  <StyledTableCell>{miscClasses[index]}</StyledTableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </ThemeProvider>
     </div>
   );
 }
